10b68c7963879526cc8adcfb3aefb1f4ff4cb9a8
Ayush <ranjanayush03@gmail.com>
2020-03-13 18:43:46 +0530

    added test cases

--- js-exercises/update-object/updateObject.test.js
+++ js-exercises/update-object/updateObject.test.js
@@ -1,3 +1,11 @@
 import { updateObject } from "./updateObject";
 
-describe("updateObject", () => {});
+describe("updateObject", () => {
+    it('should replace the existing array',() => {
+        expect(updateObject(1, '_', ['a', 'b', 'c']).toBe(['a', '_', 'c']))
+    })
+    it('should replace the existing array',() => {
+        expect(updateObject(-1, '_', ['a', 'b', 'c']).toBe(['a', 'b', '_']))
+    })
+
+});
